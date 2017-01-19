3c8150ac43a79cb0c0d679e88c51e737d4aacdf0
workPC <milboy2012@gmail.com>
2017-01-19 14:08:04 +0300

    imitateRepository

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository productsRepository)
+        {
+            this.repository = productsRepository;
+        }
+        // GET: Product
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using Moq;
+using SportsStore.Domain;
+using SportsStore.Domain.Abstract;
+
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    // реализация пользовательской фабрики контроллеров,
+    // наследуясь от фабрики используемой по умолчанию
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            // создание контейнера
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext,
+        Type controllerType)
+        {
+            // получение объекта контроллера из контейнера
+            // используя его тип
+            return controllerType == null
+            ? null
+            : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            // конфигурирование контейнера
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+    new Product { Name = "Football", Price = 25 },
+    new Product { Name = "Surf board", Price = 179 },
+    new Product { Name = "Running shoes", Price = 95 }
+    }.AsQueryable());
+            ninjectKernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+        }
+    }
+}
